279625f2b8c22f49924dfa2e76daeef069d3e9e4
Anna Hilla <annahilla21@gmail.com>
2024-10-30 19:14:00 +0100

    Add styles to cards

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,8 @@
 import Card from './components/Card'
 import { useState } from 'react'
+import timeManagementImage from './assets/time_managment.svg';
+import programmingImage from './assets/programming.svg';
+import meditationImage from './assets/meditation.svg';
 import './App.css'
 
 function App() {
@@ -9,20 +12,20 @@ function App() {
     {
       title: "Dedica moltes hores",
       description: "Un mínim de 30 hores a la setmana. Si no en tens prou, hauràs de dedicar-li més hores. Al principi sembla impossible, però notaràs una millora ràpidament.",
-      bgColor: 'blue',
-      image: ''
+      bgColor: "#4da2a9",
+      image: timeManagementImage
     },
     {
       title: "Programa projectes propis",
       description: "Més val 10 hores treballant en projectes propis, que 10 hores mirant tutorials. La motivació i la implicació en el projecte ajudarà a accelerar el teu aprenentatge.",
-      bgColor: 'gray',
-      image: ''
+      bgColor: "#d3d4d9",
+      image: programmingImage
     },
     {
       title: "Procura descansar",
       description: "Descansar bé i desconnectar són vitals. D'aquesta manera reduiràs l'estrès i l'ansietat. Milloraràs la teva concentració i consol·lidaràs el teu aprenentatge.",
-      bgColor: 'blue',
-      image: ''
+      bgColor: "#ffd167",
+      image: meditationImage
     },
   ]
 
--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,9 +1,20 @@
+import { IoMdArrowForward } from "react-icons/io";
+
 const Card = ({ currentCardData, nextStep }) => {
+    const bgColor = currentCardData.bgColor;
+
     return (
-        <div>
-            <h2>{currentCardData.title}</h2>
-            <p>{currentCardData.description}</p>
-            <button onClick={nextStep}>Next</button>
+        <div className="card-container">
+            <div className="card">
+                <img className="card-img" src={currentCardData.image} style={{ backgroundColor: bgColor }} alt={currentCardData.title} />
+                <div className="card-text">
+                    <h2>{currentCardData.title}</h2>
+                    <p>{currentCardData.description}</p>
+                </div>
+                <button className="btn" onClick={nextStep}>
+                    <IoMdArrowForward />
+                </button>
+            </div>
         </div>
     )
 }
