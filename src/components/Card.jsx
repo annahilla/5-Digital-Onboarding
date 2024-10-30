1de8066832d95314d636caeaa0dd393b27a879a2
Anna Hilla <annahilla21@gmail.com>
2024-10-30 21:30:19 +0100

    Implement progress steps dynamically

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -3,7 +3,14 @@ import { IoArrowBack } from "react-icons/io5";
 
 const Card = ({ currentCardData, tutorialData, step, nextStep, prevStep }) => {
     const bgColor = currentCardData.bgColor;
-    console.log(step);
+
+    // const progressStep1 = step === 0 ? 'progress active' : 'progress';
+    // const progressStep2 = step === 1 ? 'progress active' : 'progress';
+    // const progressStep3 = step === 2 ? 'progress active' : 'progress';
+
+    const progressSteps = tutorialData.map((_, index) => (
+        <div key={index} className={step === index ? 'progress active' : 'progress'}></div>
+    ))
 
     return (
         <div className="card-container">
@@ -13,20 +20,25 @@ const Card = ({ currentCardData, tutorialData, step, nextStep, prevStep }) => {
                     <h2>{currentCardData.title}</h2>
                     <p>{currentCardData.description}</p>
                 </div>
-                <div className="btn-container">
-                    {
-                        step !== 0
-                        && <button className="btn btn-prev" onClick={prevStep}>
-                            <IoArrowBack />
-                        </button>
+                <div className="card-footer">
+                    <div className="progress-container">
+                        {progressSteps}
+                    </div>
+                    <div className="btn-container">
+                        {
+                            step !== 0
+                            && <button className="btn btn-prev" onClick={prevStep}>
+                                <IoArrowBack />
+                            </button>
 
-                    }
-                    {
-                        step !== tutorialData.length - 1
-                        && <button className="btn btn-next" onClick={nextStep}>
-                            <IoMdArrowForward />
-                        </button>
-                    }
+                        }
+                        {
+                            step !== tutorialData.length - 1
+                            && <button className="btn btn-next" onClick={nextStep}>
+                                <IoMdArrowForward />
+                            </button>
+                        }
+                    </div>
                 </div>
             </div>
         </div>
